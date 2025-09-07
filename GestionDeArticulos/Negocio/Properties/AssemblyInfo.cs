1437b9ee19bbe85ebc9b6d4431bb58f66a3ef85c
Franco Kaner Loy <franco@gmail.com>
2025-09-07 18:00:49 -0300

    Se agrego Clase Marca + orden de capas

--- GestionDeArticulos/Negocio/Properties/AssemblyInfo.cs
+++ GestionDeArticulos/Negocio/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // La información general de un ensamblado se controla mediante el siguiente 
 // conjunto de atributos. Cambie estos valores de atributo para modificar la información
 // asociada con un ensamblado.
-[assembly: AssemblyTitle("Dominio")]
+[assembly: AssemblyTitle("Negocio")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Dominio")]
+[assembly: AssemblyProduct("Negocio")]
 [assembly: AssemblyCopyright("Copyright ©  2025")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
-[assembly: Guid("be8d7c86-a88a-42c4-82bf-75dae25900a3")]
+[assembly: Guid("fa5436db-3cb5-43aa-9888-3e4019ca888d")]
 
 // La información de versión de un ensamblado consta de los cuatro valores siguientes:
 //
