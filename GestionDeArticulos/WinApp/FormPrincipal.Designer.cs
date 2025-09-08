ef2d23b5685b74e94b6a6091425be6f01905a117
Franco Kaner Loy <franco@gmail.com>
2025-09-08 19:25:28 -0300

    Se agregaron dos botones y una panta listado Articulos

--- GestionDeArticulos/WinApp/FormPrincipal.Designer.cs
+++ GestionDeArticulos/WinApp/FormPrincipal.Designer.cs
@@ -28,21 +28,66 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnListadoArticulo = new System.Windows.Forms.Button();
+            this.lblGesDeArticulos = new System.Windows.Forms.Label();
+            this.btnBuscarArticulo = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // btnListadoArticulo
+            // 
+            this.btnListadoArticulo.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.btnListadoArticulo.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Cyan;
+            this.btnListadoArticulo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnListadoArticulo.Location = new System.Drawing.Point(226, 127);
+            this.btnListadoArticulo.Name = "btnListadoArticulo";
+            this.btnListadoArticulo.Size = new System.Drawing.Size(117, 37);
+            this.btnListadoArticulo.TabIndex = 0;
+            this.btnListadoArticulo.Text = "Listado de Articulos";
+            this.btnListadoArticulo.UseVisualStyleBackColor = true;
+            // 
+            // lblGesDeArticulos
+            // 
+            this.lblGesDeArticulos.AutoSize = true;
+            this.lblGesDeArticulos.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGesDeArticulos.Location = new System.Drawing.Point(148, 56);
+            this.lblGesDeArticulos.Name = "lblGesDeArticulos";
+            this.lblGesDeArticulos.Size = new System.Drawing.Size(285, 31);
+            this.lblGesDeArticulos.TabIndex = 1;
+            this.lblGesDeArticulos.Text = "Gestion de Articulos ";
+            // 
+            // btnBuscarArticulo
+            // 
+            this.btnBuscarArticulo.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.btnBuscarArticulo.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Cyan;
+            this.btnBuscarArticulo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnBuscarArticulo.Location = new System.Drawing.Point(226, 181);
+            this.btnBuscarArticulo.Name = "btnBuscarArticulo";
+            this.btnBuscarArticulo.Size = new System.Drawing.Size(117, 32);
+            this.btnBuscarArticulo.TabIndex = 2;
+            this.btnBuscarArticulo.Text = "Buscar Articulo";
+            this.btnBuscarArticulo.UseVisualStyleBackColor = true;
             // 
             // FormPrincipal
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(618, 450);
+            this.ClientSize = new System.Drawing.Size(618, 574);
+            this.Controls.Add(this.btnBuscarArticulo);
+            this.Controls.Add(this.lblGesDeArticulos);
+            this.Controls.Add(this.btnListadoArticulo);
             this.MinimumSize = new System.Drawing.Size(634, 489);
             this.Name = "FormPrincipal";
             this.Text = "Gestion de Articulos ";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnListadoArticulo;
+        private System.Windows.Forms.Label lblGesDeArticulos;
+        private System.Windows.Forms.Button btnBuscarArticulo;
     }
 }
 
---
+++ GestionDeArticulos/WinApp/ListadoArticulos.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace WinApp
+{
+    partial class ListadoArticulos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvListadoArticulo = new System.Windows.Forms.DataGridView();
+            this.lblListadoArticulo = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvListadoArticulo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvListadoArticulo
+            // 
+            this.dgvListadoArticulo.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvListadoArticulo.Location = new System.Drawing.Point(24, 90);
+            this.dgvListadoArticulo.Name = "dgvListadoArticulo";
+            this.dgvListadoArticulo.Size = new System.Drawing.Size(747, 330);
+            this.dgvListadoArticulo.TabIndex = 0;
+            // 
+            // lblListadoArticulo
+            // 
+            this.lblListadoArticulo.AutoSize = true;
+            this.lblListadoArticulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblListadoArticulo.Location = new System.Drawing.Point(268, 26);
+            this.lblListadoArticulo.Name = "lblListadoArticulo";
+            this.lblListadoArticulo.Size = new System.Drawing.Size(256, 31);
+            this.lblListadoArticulo.TabIndex = 1;
+            this.lblListadoArticulo.Text = "Listado de Articulo";
+            // 
+            // ListadoArticulos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblListadoArticulo);
+            this.Controls.Add(this.dgvListadoArticulo);
+            this.Name = "ListadoArticulos";
+            this.Text = "ListadoArticulos";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvListadoArticulo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvListadoArticulo;
+        private System.Windows.Forms.Label lblListadoArticulo;
+    }
+}
