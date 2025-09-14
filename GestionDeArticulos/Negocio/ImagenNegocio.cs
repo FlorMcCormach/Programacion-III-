903a6bde6e3fcec993648f7ba0ab3af239a81c4c
Emanuel Infante <pei2688@gmail.com>
2025-09-13 22:35:08 -0300

    se agrega ImagenNegocio y metodo modificar

---
+++ GestionDeArticulos/Negocio/ImagenNegocio.cs
@@ -0,0 +1,29 @@
+﻿using Dominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Negocio
+{
+    public class ImagenNegocio
+    {
+        public void modificar(Imagen imagen)
+        {
+            AccesoDatos accesoDatos = new AccesoDatos();
+            try
+            {
+                accesoDatos.setearConsulta("UPDATE IMAGENES SET ImagenUrl = @ImagenUrl WHERE Id = @Id");
+                accesoDatos.setearParametros("@ImagenUrl", imagen.ImagenUrl);
+                accesoDatos.setearParametros("@Id", imagen.IdImagen);
+                accesoDatos.ejecutarAccion();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+    }
+}
